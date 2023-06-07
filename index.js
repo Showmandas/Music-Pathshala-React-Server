36f0fa37030b6c9dee2b144f81a31991daf2e9b2
Showman_Das <Showmandas3216@gmail.com>
2023-06-08 02:12:06 +0600

    solved bad auth issue

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express=require('express')
 const app=express()
 const cors=require('cors')
+require("dotenv").config();
 const port=process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-
-//musicSchool
-//xXFHSvOBq0Tw8crq
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
