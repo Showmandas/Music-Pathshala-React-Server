6db9a28ea2ad5f699fc8e8959569b55d555f2c97
Showman_Das <Showmandas3216@gmail.com>
2023-06-12 15:06:36 +0600

    some apis added

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 // verify token
 const verifyJwt = (req, res, next) => {
   const authorization = req.headers.authorization;
+  console.log(authorization);
   if (!authorization) {
     return res
       .status(401)
@@ -23,7 +24,7 @@ const verifyJwt = (req, res, next) => {
     if (err) {
       return res
         .status(401)
-        .send({ error: true, message: "unauthorized access" });
+        .send({ error: true, message: "unauthorized access to" });
     }
     req.decoded = decoded;
     next();
@@ -61,15 +62,77 @@ async function run() {
       res.send({ token });
     });
 
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "instructor") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
     // users related apis
-    app.get("/users",verifyJwt, async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
+    app.get(
+      "/users",
+      verifyJwt,
+      verifyAdmin,
+      verifyInstructor,
+      async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      }
+    );
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+    //instructors added class apis
+    app.post("/classes", async (req, res) => {
+      const addClass = req.body;
+      console.log(addClass);
+      const result = await classCollection.insertOne(addClass);
+      res.send(result);
+    });
+
+    // show instructors added classes
+    app.get("/myaddclass", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Access is Forbidden" });
+      }
+
+      const query = { email: email };
+            const result = await classCollection.find(query).toArray();
+
+      res.send(result);
+      // console.log(result)
     });
 
     // create admin api
@@ -133,18 +196,29 @@ async function run() {
       res.send(result);
     });
 
-
     //verify admin by user email
-    app.get('/users/admin/:email',verifyJwt,async(req,res)=>{
-      const email=req.params.email;
-      if(req.decoded.email !== email){
-        res.send({admin:false})
+    app.get("/users/admin/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
       }
-      const query={email:email}
-      const user =await usersCollection.findOne(query);
-      const result={admin:user?.role==='admin'}
-      res.send(result)
-    })
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    //verify instructor by user email
+    app.get("/users/instructor/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
 
     //create seats
     app.post("/seats", async (req, res) => {
